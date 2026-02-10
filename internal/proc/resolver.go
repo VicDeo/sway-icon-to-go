561862ca8839a0c9927ce226dfc37ba4a0750fa2
VicDeo <victor.dubiniuk@gmail.com>
2026-02-10 23:00:27 +0100

    Document resolver

--- internal/proc/resolver.go
+++ internal/proc/resolver.go
@@ -5,10 +5,12 @@ import (
 	"strconv"
 )
 
+// LinuxResolver is a struct that resolves the process name by pid.
 type LinuxResolver struct {
 	ProcPath string
 }
 
+// Resolve resolves the process name by pid.
 func (l *LinuxResolver) Resolve(pid uint32) (string, error) {
 	exePath := filepath.Join(l.ProcPath, strconv.FormatUint(uint64(pid), 10), "exe")
 	realPath, err := filepath.EvalSymlinks(exePath)
